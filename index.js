8287e47ca4aafebbda4a4f8c33b4ea8847fbcef1
nerellaprathyusha <113548138+nerellaprathyusha@users.noreply.github.com>
2022-09-28 21:27:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-const http=require("http");
-const fs=require("fs");
-let homecontent="";
-let projectcontent="";
-let registrationcontent="";
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
 const args = require("minimist")(process.argv.slice(2));
-fs.readFile("home.html",(err,home)=>{
-    if(err){ throw err};
-    homecontent=home.toString();
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
 });
-fs.readFile("project.html",(err,project)=>{
-    if(err){
-        throw err;
-    }
-    projectcontent=project.toString();
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
 });
-fs.readFile("registration.html",(err,registration)=>{
-    if(err){
-        throw err;
-    }
-    registrationcontent=registration.toString();
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
 });
-http.createServer((req,res)=>{
-    let url=req.url;
-    res.writeHead(200,{"content-type":"text/html"});
-    switch(url)
-    {
-        case "/project":
-            res.write(projectcontent);
-            res.end();
-            break;
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
         case "/registration":
-            res.write(registrationcontent);
-            res.end();
-            break;
-        default:res.write(homecontent);
-        res.end();
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
         break;
     }
 }).listen(args["port"]);
